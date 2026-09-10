c619bfebcc5ef6cea3618d71a6cdad37aadf7c74
NitsuaMa <jurgensaj@gmail.com>
2026-09-10 18:09:23 -0400

    History: wire the new tab into the profile; retire the 1,775-line calendar (the profile's event form also takes the Eastern day)

---
+++ src/features/client-history/index.ts
@@ -0,0 +1,7 @@
+export { ClientHistoryTab, type ClientHistoryTabProps } from "./ClientHistoryTab";
+export { HistoryView, type HistoryViewProps, type HistoryViewMode } from "./HistoryView";
+export { SessionDetailDialog } from "./SessionDetailDialog";
+export { LogPastSessionDialog } from "./LogPastSessionDialog";
+export { useSessionHistory, useSessionLogs, FIRST_WINDOW, FULL_WINDOW } from "./useSessionHistory";
+export { trainerLookup } from "./trainers";
+export * from "./model";
